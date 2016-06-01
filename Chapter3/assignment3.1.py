84a0b7cc0d90dd6cac3416b617c81432f815ca92
chennan625 <chennan625@gmail.com>
2016-05-31 22:07:16 -0500

    chapter 3 assignments

---
+++ Chapter3/assignment3.1.py
@@ -0,0 +1,12 @@
+'''
+Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+'''
+hrs = raw_input("Enter Hours:")
+h = float(hrs)
+rate = raw_input("Enter Rate:")
+r = float(rate)
+
+if( h > 40.0):
+    print 'Pay:', 40.0 * r + (h - 40.0) * r * 1.5
+else:
+    print 'Pay:', h * r
---
+++ Chapter3/assignment3.2.py
@@ -0,0 +1,17 @@
+'''
+Rewrite your pay program using TRY and EXCEPT so that your program handles non-numeric input
+gracefully by printing a message and exiting the program.
+'''
+try:
+    hrs = raw_input("Enter Hours:")
+    h = float(hrs)
+    rate = raw_input("Enter Rate:")
+    r = float(rate)
+except:
+    print 'Error, please enter numeric input'
+    quit()
+
+if( h > 40.0):
+    print 'Pay:', 40.0 * r + (h - 40.0) * r * 1.5
+else:
+    print 'Pay:', h * r
