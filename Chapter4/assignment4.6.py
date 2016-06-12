3b162518f71f2825fd4e9268c07b0be9d6551679
chennan625 <chennan625@gmail.com>
2016-06-12 01:38:40 -0500

    Chapter 4 assignments

---
+++ Chapter4/assignment4.6.py
@@ -0,0 +1,14 @@
+'''
+Rewrite your pay computation with time-and-a-half for overtime and create a function called computepay which takes two parameters.
+'''
+def computepay(h,r):
+    if( h > 40.0):
+        return 40.0 * r + (h - 40.0) * r * 1.5
+    else:
+        return h * r
+
+hrs = raw_input("Enter Hours:")
+rate = raw_input("Enter Rate:")
+p = computepay(float(hrs),float(rate))
+
+print "Pay:", p
---
+++ Chapter4/assignment4.7.py
@@ -0,0 +1,26 @@
+'''
+Rewrite the grade program from the previous chapter using a function called computegrade that takes a score 
+as its parameter and return a grade as a string
+'''
+
+def computegrade(score):
+
+    if(score > 0.0 and score < 1.0):
+        if(score >= 0.9):
+            return 'A'
+        elif(score >= 0.8):
+            return 'B'
+        elif(score >= 0.7):
+            return 'C'
+        elif(score >= 0.6):
+            return 'D'
+        else:
+            return 'F'
+    else:
+        return 'Bad score.'
+
+
+try:
+    print computegrade(float(raw_input("Enter Score: ")))
+except:
+    print 'Bad score.'
